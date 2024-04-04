6d20288a19a96edaba96b8b46c820f5dff8cd9f4
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-04 13:19:08 -0500

    add mongoose to app.js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-require('dotenv').config();
-
-
-
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.error('Connection error', err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
