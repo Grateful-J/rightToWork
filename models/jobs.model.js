e87d0c2ce8bf840e6e38196c197ec1802a814cee
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-04 22:04:19 -0400

    add require rtwStates to model

--- models/jobs.model.js
+++ models/jobs.model.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
+const rtwStates = require("./rtwStates"); // pulls array of Right to Work States
 
 const jobSchema = new mongoose.Schema({
-    jobname: String,
-    client: String,
-    location: String,
-    startDate: Date,
-    endDate: Date,
-    travelDays: Number,
-    isRTW: {type:Boolean, default: false},
+  jobname: String,
+  client: String,
+  location: String,
+  startDate: Date,
+  endDate: Date,
+  travelDays: Number,
+  isRTW: { type: Boolean, default: false },
 });
 
 //Pre-save hook to set isRTW to "true"
-jobSchema.pre('save', function(next){
-    this.isRTW = rtwStates.includes(this.location);
-    next();
+jobSchema.pre("save", function (next) {
+  this.isRTW = rtwStates.includes(this.location);
+  next();
 });
 
+const Job = mongoose.model("Job", jobSchema);
+
+module.exports = Job;
