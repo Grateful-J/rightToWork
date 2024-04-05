97809e5e09abc5d0b9e9a1b0afdbff9c00868ad6
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-04 20:14:22 -0400

    commented out mongoDB.troubleshooting w/ boiler

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-const express = require('express');
-require('dotenv').config();
+
+
+/* require('dotenv').config();
 const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); */
 
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
 
-// Connect to MongoDB using Mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Successfully connected to MongoDB'))
-  .catch(err => console.error('Connection error', err));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+app.listen(3000, ()=> {
+    console.log('Server is running, yo! on port 3000')
+})
