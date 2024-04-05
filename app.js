509a677d9304fdd02dbfc78409bffbdcc795fc9c
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-04 21:02:19 -0400

    added secret for mongo

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-
-
-/* require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); */
-
 const express = require('express')
 const app = express()
+require('dotenv').config();
 
-app.listen(3000, ()=> {
-    console.log('Server is running, yo! on port 3000')
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log( "Connected to Database");
+    app.listen(3000, ()=> {
+        console.log('Server is running, yo! on port 3000')
+    })
 })
+
+.catch(()=> {
+    console.log("Error: Connecting to Database");
+})
+
+
 
 app.get('/', (req, res) =>{
-    res.send('Hello from node API!');
+    res.send('Hello from node API! updated x3');
 })
+
