265e2c68a3d4584d78077718b6060cd201cf3777
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-06 11:09:31 -0400

    added API patch routes

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Product = require('./models/product.model');
-const Job = require('./models/jobs.model');
+const Product = require("./models/product.model");
+const Job = require("./models/jobs.model");
 require("dotenv").config();
 
 // Middle-ware for JSON in API
 app.use(express.json());
-
-// Example: Creating a new job
-const newJob = new Job({
-    jobName: "Example Job",
-    client: "Example Client",
-    location: "Arizona",
-    startDate: new Date(),
-    endDate: new Date(),
-    travelDays: 2,
-  });
-  
-  newJob.save()
-    .then(savedJob => console.log(savedJob))
-    .catch(err => console.error(err));
 
 // API "GET" Commands
 app.get("/", (req, res) => {
   res.send("Hello from node API! updated");
 });
 
-// API "GET" Products
-app.get("/api/products", async (req, res) => {
-    try{
-        const products = await Product.find({});
-        res.status(200).json(products)
-
-    } catch(error) {
-        res.status(500).json({message: error.message});
-
-    }
+// GET all jobs
+app.get("/api/jobs", async (req, res) => {
+  try {
+    const jobs = await Job.find({});
+    res.status(200).json(jobs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-// API "POST" Commands
-app.post("/api/products",async (req, res) => {
+// POST new job
+app.post("/api/jobs", async (req, res) => {
   try {
-    const product = await Product.create(req.body) // creates new product from response
-    res.status(200).json({product});
-
+    const job = new Job(req.body);
+    const newJob = await job.save(); // Saves new job
+    res.status(201).json(newJob); //Return the saved job, status 201 for created
   } catch (error) {
-    res.status(500).json({message: error.message});
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Updates new job
+app.patch("api/jobs/:id", async (req, res) => {
+  try {
+    const job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
