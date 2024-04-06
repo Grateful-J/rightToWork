2d3142c79f67812347a721b37de13666b2f90a9d
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-06 15:25:55 -0400

    clean up front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import './App.css'; // Keeps the default styling
+// Import your custom styles
+import './styles.css';
+
+import JobList from './components/JobList';
+import JobForm from './components/JobForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Job Tracker</h1>
+            </header>
+            <main>
+                <JobForm onSave={job => console.log(job)} />
+                <JobList />
+            </main>
+        </div>
+    );
 }
 
 export default App;
