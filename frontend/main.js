089db281d18ff780503a7a8b9c120132bf50ab38
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-07 13:37:01 -0400

    directed addJobs to localhost

--- frontend/main.js
+++ frontend/main.js
@@ -23,7 +23,7 @@ async function addJob(event) {
   };
 
   try {
-    const response = await fetch("./api/jobs", {
+    const response = await fetch('http://localhost:3000/api/jobs', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
