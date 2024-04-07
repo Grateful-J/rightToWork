db0a0216f51af621a7dccc7ec7c1edb359769099
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-07 10:42:48 -0400

    create jobRoutes

---
+++ models/routes/jobRoutes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const Job = require("../../models/jobs.model");
+
+//GET all jobs
+router.get("/", async (req, res) => {
+    try {
+        const jobs = await Job.find({});
+        res.status(200).json(jobs);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//POST new job
+router.post("/", async (req, res) => {
+    const newJob = new Job(req.body);
+    try {
+        const savedJob = await newJob.save();
+        res.status(201).json(savedJob);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//PATCH jobs
+router.patch("/:id", async (req, res) => {
+    try {
+        const updatedJob = await Job.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        res.status(200).json(updatedJob);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//DELETE job
+router.delete("/:id", async (req, res) => {
+    try {
+        const deletedJob = await Job.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedJob);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+module.exports = router;
