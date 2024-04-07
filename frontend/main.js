909bedab5d715b09bf0e6e82a8988d772d9f9ab4
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-07 11:21:20 -0400

    displayJobs on main.js

--- frontend/main.js
+++ frontend/main.js
@@ -13,3 +13,21 @@ async function fetchJobs() {
     console.error("Failed to fetch jobs", error);
   }
 }
+
+function displayJobs(jobs) {
+  const container = document.querySelector("#jobs-container");
+  container.innerHTML = '', //clear exisiting jobs
+  jobs.forEach((job) => {
+    const jobDiv = document.createElement("div");
+    jobDiv.innerHTML = `
+      <h3>${job.jobName}</h3>
+      <p>Client: ${job.client}</p>
+      <p>Location: ${job.location}</p>
+      <p>Start Date: ${job.startDate}</p>
+      <p>End Date: ${job.endDate}</p>
+      <p>Travel Days: ${job.travelDays}</p>
+      <p>RTW: ${job.isRTW ? "Yes" : "No"}</p>
+    `;
+    container.appendChild(jobDiv);
+  });
+}
