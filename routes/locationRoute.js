e6760d934e9be809be7014b284cdcb67cf509f4a
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-04-12 08:17:08 -0500

    location router complete

--- routes/locationRoute.js
+++ routes/locationRoute.js
@@ -1,3 +1,56 @@
 const express = require("express");
 const router = express.Router();
 const Location = require("../models/location.model");
+
+//GET all locations
+router.get("/", async (req, res) => {
+  try {
+    const locations = await Location.find({});
+    res.status(200).json(locations);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//POST new location
+router.post("/", async (req, res) => {
+  const newLocation = new Location(req.body);
+  try {
+    const savedLocation = await newLocation.save();
+    res.status(201).json(savedLocation);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//PATCH location
+router.patch("/:id", async (req, res) => {
+  try {
+    const updatedLocation = await Location.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    res.status(200).json(updatedLocation);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//PUT location
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedLocation = await Location.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    res.status(200).json(updatedLocation);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//DELETE location
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedLocation = await Location.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedLocation);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
